1c569617ccf1142377857f8f6c28e708a836cab7
Your Name <you@example.com>
2019-12-06 14:19:11 -0800

    Disable code objects moving in v8 GC for CET.

--- src/flags/flag-definitions.h
+++ src/flags/flag-definitions.h
@@ -947,7 +947,7 @@ DEFINE_INT(v8_os_page_size, 0, "override OS page size (in KBytes)")
 DEFINE_BOOL(always_compact, false, "Perform compaction on every full GC")
 DEFINE_BOOL(never_compact, false,
             "Never perform compaction on full GC - testing only")
-DEFINE_BOOL(compact_code_space, true, "Compact code space on full collections")
+DEFINE_BOOL(compact_code_space, false, "Compact code space on full collections")
 DEFINE_BOOL(flush_bytecode, true,
             "flush of bytecode when it has not been executed recently")
 DEFINE_BOOL(stress_flush_bytecode, false, "stress bytecode flushing")
